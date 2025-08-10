f47a37b6f73b94f08ecc58c3e820e45834f144d2
mandava geetha bhargava <m.gbhargava@gmail.com>
2025-08-10 10:08:22 +0530

    updated

--- portfolio-nextjs/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto px-4 text-center">
-        <p>&copy; 2024 Created with Next.js</p>
-      </div>
-    </footer>
-  );
-}
